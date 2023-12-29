b6a8cf08f59e4e9341661b152fe2545d745f19b0
wmzqyz <154952735+wmzqyz@users.noreply.github.com>
2023-12-29 17:05:04 +0800

    Update bind_polyfill.js

--- 2048/bind_polyfill.js
+++ 2048/bind_polyfill.js
@@ -418,10 +418,10 @@ HTMLActuator.prototype.clearMessage = function () {
 HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
-  tweet.setAttribute("href", "https://twitter.com/share");
+  tweet.setAttribute("href", "https://wmzqyz.github.io/a/2048/");
   tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-url", "https://wmzqyz.github.io/a/2048/");
+  tweet.setAttribute("data-counturl", "https://wmzqyz.github.io/a/2048/");
   tweet.textContent = "Tweet";
 
   var text = "I scored " + this.score + " points at 2048, a game where you " +
